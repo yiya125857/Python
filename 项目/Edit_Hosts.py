9026410eb0c6a47b6aabace8c66686aab88b6c58
浩浩 <42765865+yiya125857@users.noreply.github.com>
2020-03-14 09:40:03 +0800

    Add files via upload

--- 项目/Edit_Hosts.py
+++ 项目/Edit_Hosts.py
@@ -1,2 +1,21 @@
-import requests
-ym="www.google.com"
+from selenium import webdriver
+from time import sleep
+
+on_off=1
+#窗口开关
+if on_off :
+    driver = webdriver.Chrome()
+else:
+    
+    from selenium.webdriver.chrome.options import Options
+    chrome_options =Options()
+    chrome_options.add_argument('headless')
+    chrome_options.add_argument('blink-settings=imagesEnabled=false') #图片加载
+    driver = webdriver.Chrome(options=chrome_options)
+
+
+driver.get("https://ping.chinaz.com/github.com")
+print(driver.title)
+
+sleep(126)
+driver.quit()
